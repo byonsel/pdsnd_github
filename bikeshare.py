0d097dbaff0d4d212f55359a06b5ea57543fffd3
Basar Yonsel <basar.yonsel@turkcell.com.tr>
2020-05-30 10:16:07 +0300

    refactor: Refactor statistics functions

--- bikeshare.py
+++ bikeshare.py
@@ -120,7 +120,7 @@ def station_stats(df):
     print(station)
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print('-'*20)
 
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
